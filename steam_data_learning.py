9cec37c6afc6790dba61e6d814098eff71d43708
Brandon <brandoneugenewalker@outlook.com>
2019-11-12 17:39:53 -0500

    Added base python file.

---
+++ steam_data_learning.py
@@ -0,0 +1,24 @@
+#/usr/local/bin/python3.7.0
+"""
+Processes data from the steam store and then uses it to learn with Linear Regression, Decision Trees, and Random Forests. 
+"""
+def steam_file_processor(file_name):
+    """
+
+    """
+
+def steam_learning_regression(data):
+    """
+
+    """
+
+def steam_learning_tree(data):
+    """
+
+    """
+
+def steam_learning_forest(data):
+    """
+
+    """
+
